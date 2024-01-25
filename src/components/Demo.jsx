c6154974452d449d63780351248d354493c436bc
danie odukwe <kingdanny295@gmail.com>
2024-01-25 15:28:55 +0100

    created the styles, now adding functionality

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,37 @@
-
+import { useState, useEffect } from "react"
+import { copy, linkIcon, loader, tick } from '../assets'
 
 const Demo = () => {
   return (
-    <div>Demo</div>
+    <section className="MT-16 w-full max-w-xl">
+        {/* search */}
+        <div className="flex flex-col w-full gap-2">
+            <form
+                className="relative flex justify-center items-center"
+                onSubmit={(e)=> e.preventDefault}
+            >
+                <img 
+                    src={linkIcon} 
+                    alt="link_icon" 
+                    className="absolute left-0 my-2 ml-3 w-5"/>
+                <input 
+                    type="url" 
+                    placeholder="Enter a URL"
+                    value=""
+                    onChange={()=>{}}
+                    required
+                    className="url_input peer"
+                />
+
+                <button
+                    type="submit"
+                    className="submit_btn"
+                >
+                    ?
+                </button>
+            </form>
+        </div>
+    </section>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,29 @@
-
+import { logo } from '../assets'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='w-full flex justify-center items-center flex-col'>
+        <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+            <img src={logo} alt='summary_logo'
+            className='w-28 object-contain'/>
+
+            <button 
+                type='button' 
+                onClick={()=> window.open('https://Vader2K')}
+                className='black_btn'>
+                Github
+            </button>
+        </nav>
+
+        <h1 className='head_text'>
+            Summarize Articles With <br className='hidden sm:block'/> 
+            <span className='orange_gradient'>OpenAI GPT_4</span>
+        </h1>
+        <h2 className='desc'>
+            Simplify your reading with summarize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries.
+        </h2>
+        
+    </div>
   )
 }
 
