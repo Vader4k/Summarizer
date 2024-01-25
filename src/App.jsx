94f324e99cd01ee336359fbd5e60939e71b118f1
danie odukwe <kingdanny295@gmail.com>
2024-01-25 15:04:27 +0100

    an ai article summarizer

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './app.css'
+
+import Hero from './components/Hero'
+import Demo from './components/Demo'
+
+const App = () => {
+  return (
+    <main>
+        
+    </main>
+  )
+}
+
+export default App
