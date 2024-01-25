da8234ea4afb2eb017c0a00e0d1bd35eb39f905a
danie odukwe <kingdanny295@gmail.com>
2024-01-25 15:37:39 +0100

    testing the submit button

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,13 +2,23 @@ import { useState, useEffect } from "react"
 import { copy, linkIcon, loader, tick } from '../assets'
 
 const Demo = () => {
+
+    const [ article, setArticle ] = useState({
+        url: '',
+        summary: '',
+    })
+
+    const handleSubmit = async (e) => {
+        e.preventDefault
+        alert('submitted')
+    }
   return (
-    <section className="MT-16 w-full max-w-xl">
+    <section className="mt-16 w-full max-w-xl">
         {/* search */}
         <div className="flex flex-col w-full gap-2">
             <form
                 className="relative flex justify-center items-center"
-                onSubmit={(e)=> e.preventDefault}
+                onSubmit={handleSubmit}
             >
                 <img 
                     src={linkIcon} 
@@ -17,20 +27,28 @@ const Demo = () => {
                 <input 
                     type="url" 
                     placeholder="Enter a URL"
-                    value=""
-                    onChange={()=>{}}
+                    value={article.url}
+                    onChange={(e)=> setArticle({
+                        ...article, url:e.target.value
+                    })}
                     required
                     className="url_input peer"
                 />
 
                 <button
                     type="submit"
-                    className="submit_btn"
+                    className="submit_btn
+                    peer-focus:border-gray-700
+                    peer-focus:text-gray-700"                    
                 >
                     ?
                 </button>
             </form>
+
+            {/* browser url history */}
         </div>
+
+        {/* display results */}
     </section>
   )
 }
