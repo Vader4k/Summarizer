94f090a4e320fb1f8f243d4fd996fa6359f658c2
danie odukwe <kingdanny295@gmail.com>
2024-01-26 18:13:04 +0100

    made few imports to the demo

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -13,7 +13,7 @@ const Demo = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const { data } = await getSummary({ articleUrl: article.url})
+        const { data }  = await getSummary({ articleUrl: article.url})
 
         if(data?.summary) {
             const newArticle = { ...article, summary: data.summary }
